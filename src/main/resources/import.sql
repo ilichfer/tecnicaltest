b737a258c428f1a97bb55f5a53d5e74bdc696961
ilichfer <ilichferv@hotmail.com>
2025-03-23 21:36:29 -0500

    insert data into DB

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO price (id, brand_id, product_id, price_list, start_date, end_date, price,curr,priority) VALUES (1,1, 35455, 1, '2020-06-14 00:00:00', '2020-12-31 23:59:59', 35.50,'EUR',0);
+INSERT INTO price (id, brand_id, product_id, price_list, start_date, end_date, price,curr,priority) VALUES (2,1, 35455, 2, '2020-06-14 15:00:00', '2020-06-14 18:30:00', 25.45,'EUR',1);
+INSERT INTO price (id, brand_id, product_id, price_list, start_date, end_date, price,curr,priority) VALUES (3,1, 35455, 3, '2020-06-15 00:00:00', '2020-06-15 11:00:00', 30.50,'EUR',1);
+INSERT INTO price (id, brand_id, product_id, price_list, start_date, end_date, price,curr,priority) VALUES (4,1, 35455, 4, '2020-06-15 16:00:00', '2020-12-31 23:59:59', 38.95,'EUR',1);
